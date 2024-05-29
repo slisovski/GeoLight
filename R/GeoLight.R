1c01aadce23f1a4e4a11f6c7006561b49d17a497
Simeon Lisovski <slisovsk@PGeo07m013.dmawi.de>
2024-05-29 10:44:59 +0200

    twilightCalc message

--- R/GeoLight.R
+++ R/GeoLight.R
@@ -2013,35 +2013,36 @@ i.loxodrom.dist <- function(x1, y1, x2, y2, epsilon=0.0001){
   dis*rerde
 }
 
-i.preSelection <- function(datetime, light, LightThreshold){
+i.preSelection <- function(date, light, LightThreshold){
   
-  dt <- cut(datetime,"1 hour")
-  st <- as.POSIXct(levels(dt),"UTC")
-  
-  raw <- data.frame(datetime=dt,light=light)
-  
-  h  <- tapply(light,dt,max)
-  df1 <- data.frame(datetime=st+(30*60),light=as.numeric(h))
-  
-  smooth <- i.twilightEvents(df1[,1], df1[,2], LightThreshold)
-  smooth <- data.frame(id=1:nrow(smooth),smooth)
-  raw    <- i.twilightEvents(datetime, light, LightThreshold)
-  raw <- data.frame(id=1:nrow(raw),raw)
-  
-  ind2 <- rep(NA,nrow(smooth))
-  for(i in 1:nrow(smooth)){
-    tmp <- subset(raw,datetime>=(smooth[i,2]-(90*60)) & datetime<=(smooth[i,2]+(90*60)))
-    
-    if(smooth[i,3]==1) ind3 <- tmp$id[which.min(tmp[,2])]
-    if(smooth[i,3]==2) ind3 <- tmp$id[which.max(tmp[,2])]
-    ind2[i] <- ind3
-  }
-  
-  
-  res <- data.frame(raw,mod=1)
-  res$mod[ind2] <- 0
-  
-  return(res)
+  # dt <- cut(datetime,"1 hour")
+  # st <- as.POSIXct(levels(dt),"UTC")
+  # 
+  # raw <- data.frame(datetime=dt,light=light)
+  # 
+  # h  <- tapply(light,dt,max)
+  # df1 <- data.frame(datetime=st+(30*60),light=as.numeric(h))
+  # 
+  # smooth <- i.twilightEvents(df1[,1], df1[,2], LightThreshold)
+  # smooth <- data.frame(id=1:nrow(smooth),smooth)
+  # raw    <- i.twilightEvents(datetime, light, LightThreshold)
+  # raw <- data.frame(id=1:nrow(raw),raw)
+  # 
+  # ind2 <- rep(NA,nrow(smooth))
+  # for(i in 1:nrow(smooth)){
+  #   tmp <- subset(raw,datetime>=(smooth[i,2]-(90*60)) & datetime<=(smooth[i,2]+(90*60)))
+  #   
+  #   if(smooth[i,3]==1) ind3 <- tmp$id[which.min(tmp[,2])]
+  #   if(smooth[i,3]==2) ind3 <- tmp$id[which.max(tmp[,2])]
+  #   ind2[i] <- ind3
+  # }
+  # 
+  # 
+  # res <- data.frame(raw,mod=1)
+  # res$mod[ind2] <- 0
+  # 
+  # return(res)
+
 }
 
 i.rad <- function(Deg) {
@@ -3109,99 +3110,102 @@ tripMap <- function(crds, equinox=TRUE, xlim = NULL, ylim = NULL, legend = TRUE,
 #' @importFrom graphics abline axis identify legend plot points 
 twilightCalc <- function(datetime, light, LightThreshold=TRUE, preSelection=TRUE, maxLight=NULL, ask=TRUE, nsee=500, allTwilights=FALSE)
 {
-  if(class(datetime)[1]!="POSIXct") {
-    stop(sprintf("datetime need to be provided as POSIXct class object."), call. = F)
-    
-  } else {
-    bas <- data.frame(datetime=as.POSIXct(as.character(datetime),"UTC"),light)
-  }
   
+  message("This function is no longer supported. The R Package TwGeos has far better ways to calculate twilight times from ligth recordings.\n see https://geolocationmanual.vogelwarte.ch/ for details.")
   
-  if (is.numeric(LightThreshold))
-  {
-    LightThreshold <- as.numeric(LightThreshold)
-    min <- min(bas$light)
-  } else {
-    # Basic level
-    r <- as.data.frame(table(bas$light))
-    nr <- as.numeric(which.max(r$Freq[as.numeric(r[,1])<mean(bas$light)]))
-    LightThreshold <- (as.numeric(as.character(r[nr,1])))+3
-  }
-  
-  out <- i.preSelection(bas$datetime,bas$light, LightThreshold)[,-1]
-  
-  if(!preSelection) out$mod <- 0
-  
-  if(ask)
-  {
-    n   <- nrow(bas)
-    nn  <- n%/%nsee + 1
-    cutsub <- cut(1:n, nn)
-    picks <- NULL
-    
-    for(i in 1:nn){
-      sub <- cutsub == levels(cutsub)[i]
-      
-      repeat{
-        plot(bas[sub,1],bas[sub,2],type="o",cex=0.6,pch=20,ylab="Light intensity",xaxs="i",xaxt="n",xlab="",
-             main=paste(as.Date(min(bas[sub,1]))," to ", as.Date(max(bas[sub,1]))," (end: ",as.Date(max(bas[,1])),")",sep=""))
-        abline(h=LightThreshold,col="blue",lty=2)
-        abline(v=out[out$mod==0,1],col="orange",lty=ifelse(out[out$mod==0,2]==1,1,2))
-        points(out[,1],rep(LightThreshold,nrow(out[,])),col=ifelse(out$mod==0,"orange","grey"),pch=20,cex=0.8)
-        
-        axis(1,at=out[seq(from=1,to=nrow(out),length.out=(nrow(out)%/%2)),1],
-             labels=substring(as.character(out[seq(from=1,to=nrow(out),length.out=(nrow(out)%/%2)),1]),6,16),cex=0.7)
-        
-        legend("topright",lty=c(3,1,2),lwd=c(1.3,2,2),col=c("blue",rep("orange",2)),c("Light\nThreshold","sunrise","sunset"),cex=1,bg="white")
-        
-        nr <- identify(out[,1],rep(LightThreshold,nrow(out)),n=1,plot=F)
-        ifelse(length(nr)>0,ifelse(out$mod[nr]==0,out$mod[nr]<-1,out$mod[nr]<-0),break)
-      }
-    }
-    
-    cat("Thank you!\n\n")
-    graphics.off()
-  }
-  
-  results <- list()
-  
-  
-  out <- subset(out,out$mod==0)[,-3]
-  
-  raw <- data.frame(datetime=c(as.POSIXct(datetime,"UTC"),as.POSIXct(out$datetime,"UTC")),
-                    light=c(light,rep(LightThreshold,nrow(out))),type=c(rep(0,length(datetime)),out$type))
-  
-  raw <- raw[order(raw$type),]
-  raw <- raw[-which(duplicated(as.character(raw$datetime),fromLast=T)),]
-  raw <- raw[order(raw$datetime),]
-  
-  results$allTwilights <- raw
-  
-  opt <- data.frame(tFirst=as.POSIXct("1900-01-01 01:01","UTC"),tSecond=as.POSIXct("1900-01-01 01:01","UTC"),type=0)
-  row <- 1
-  for (k in 1:(nrow(out)-1))
-  {
-    if (as.numeric(difftime(out[k,1],out[k+1,1]))< 24 & out[k,1] != out[k+1,1])
-    {
-      opt[row,1] <- out[k,1]
-      opt[row,2] <- out[k+1,1]
-      opt[row,3] <- out$type[k]
-      
-      row <- row+1
-    }
-  }
-  
-  if(is.numeric(maxLight))
-  {
-    opt$tFirst[opt$type==2] <- opt$tFirst[opt$type==2] - (maxLight*60)
-    opt$tSecond[opt$type==1] <- opt$tSecond[opt$type==1] - (maxLight*60)
-  }
-  
-  if(allTwilights) {
-    results$consecTwilights <- opt
-    return(results)
-  } else {
-    return (opt)}
+  # if(class(datetime)[1]!="POSIXct") {
+  #   stop(sprintf("datetime need to be provided as POSIXct class object."), call. = F)
+  #   
+  # } else {
+  #   bas <- data.frame(datetime=as.POSIXct(as.character(datetime),"UTC"),light)
+  # }
+  # 
+  # 
+  # if (is.numeric(LightThreshold))
+  # {
+  #   LightThreshold <- as.numeric(LightThreshold)
+  #   min <- min(bas$light)
+  # } else {
+  #   # Basic level
+  #   r <- as.data.frame(table(bas$light))
+  #   nr <- as.numeric(which.max(r$Freq[as.numeric(r[,1])<mean(bas$light)]))
+  #   LightThreshold <- (as.numeric(as.character(r[nr,1])))+3
+  # }
+  # 
+  # out <- i.preSelection(bas$datetime,bas$light, LightThreshold)[,-1]
+  # 
+  # if(!preSelection) out$mod <- 0
+  # 
+  # if(ask)
+  # {
+  #   n   <- nrow(bas)
+  #   nn  <- n%/%nsee + 1
+  #   cutsub <- cut(1:n, nn)
+  #   picks <- NULL
+  #   
+  #   for(i in 1:nn){
+  #     sub <- cutsub == levels(cutsub)[i]
+  #     
+  #     repeat{
+  #       plot(bas[sub,1],bas[sub,2],type="o",cex=0.6,pch=20,ylab="Light intensity",xaxs="i",xaxt="n",xlab="",
+  #            main=paste(as.Date(min(bas[sub,1]))," to ", as.Date(max(bas[sub,1]))," (end: ",as.Date(max(bas[,1])),")",sep=""))
+  #       abline(h=LightThreshold,col="blue",lty=2)
+  #       abline(v=out[out$mod==0,1],col="orange",lty=ifelse(out[out$mod==0,2]==1,1,2))
+  #       points(out[,1],rep(LightThreshold,nrow(out[,])),col=ifelse(out$mod==0,"orange","grey"),pch=20,cex=0.8)
+  #       
+  #       axis(1,at=out[seq(from=1,to=nrow(out),length.out=(nrow(out)%/%2)),1],
+  #            labels=substring(as.character(out[seq(from=1,to=nrow(out),length.out=(nrow(out)%/%2)),1]),6,16),cex=0.7)
+  #       
+  #       legend("topright",lty=c(3,1,2),lwd=c(1.3,2,2),col=c("blue",rep("orange",2)),c("Light\nThreshold","sunrise","sunset"),cex=1,bg="white")
+  #       
+  #       nr <- identify(out[,1],rep(LightThreshold,nrow(out)),n=1,plot=F)
+  #       ifelse(length(nr)>0,ifelse(out$mod[nr]==0,out$mod[nr]<-1,out$mod[nr]<-0),break)
+  #     }
+  #   }
+  #   
+  #   cat("Thank you!\n\n")
+  #   graphics.off()
+  # }
+  # 
+  # results <- list()
+  # 
+  # 
+  # out <- subset(out,out$mod==0)[,-3]
+  # 
+  # raw <- data.frame(datetime=c(as.POSIXct(datetime,"UTC"),as.POSIXct(out$datetime,"UTC")),
+  #                   light=c(light,rep(LightThreshold,nrow(out))),type=c(rep(0,length(datetime)),out$type))
+  # 
+  # raw <- raw[order(raw$type),]
+  # raw <- raw[-which(duplicated(as.character(raw$datetime),fromLast=T)),]
+  # raw <- raw[order(raw$datetime),]
+  # 
+  # results$allTwilights <- raw
+  # 
+  # opt <- data.frame(tFirst=as.POSIXct("1900-01-01 01:01","UTC"),tSecond=as.POSIXct("1900-01-01 01:01","UTC"),type=0)
+  # row <- 1
+  # for (k in 1:(nrow(out)-1))
+  # {
+  #   if (as.numeric(difftime(out[k,1],out[k+1,1]))< 24 & out[k,1] != out[k+1,1])
+  #   {
+  #     opt[row,1] <- out[k,1]
+  #     opt[row,2] <- out[k+1,1]
+  #     opt[row,3] <- out$type[k]
+  #     
+  #     row <- row+1
+  #   }
+  # }
+  # 
+  # if(is.numeric(maxLight))
+  # {
+  #   opt$tFirst[opt$type==2] <- opt$tFirst[opt$type==2] - (maxLight*60)
+  #   opt$tSecond[opt$type==1] <- opt$tSecond[opt$type==1] - (maxLight*60)
+  # }
+  # 
+  # if(allTwilights) {
+  #   results$consecTwilights <- opt
+  #   return(results)
+  # } else {
+  #   return (opt)}
 }
 
 #' Example data for calibration: Light intensities and twilight events
